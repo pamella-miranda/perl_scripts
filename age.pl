b4fd8be894dc5dc83bccee2c03e5837785ebcab7
Pamella Miranda <129092835+pamella-miranda@users.noreply.github.com>
2023-09-25 16:28:10 -0300

    legal age or not

---
+++ age.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+#perl age.pl
+
+#enter the person's age
+#receber a idade da pessoa
+#print majority or not
+
+use strict;
+use warnings;
+
+print "Enter your age: ";
+my $id = <>;
+chomp $id;
+
+if ($id >= 18){
+    print "You are of legal age!\n";
+    }else{
+        print "You are not of legal age!\n";
+        }
