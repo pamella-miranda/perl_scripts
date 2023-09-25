2310c931fb8ea83b08418a53b9a26620309a43d3
Pamella Miranda <129092835+pamella-miranda@users.noreply.github.com>
2023-09-25 16:31:22 -0300

    approved or not approved

---
+++ grade.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+
+#perl grade.pl
+
+#enter student grade
+#print approved or not approved
+#average grade 7
+
+use strict;
+use warnings;
+
+print "Enter your grade: ";
+my $nota = <>;
+chomp $nota;
+
+if ($nota < 7){
+    print "You are not approved!\n";
+    }elsif ($nota > 7){
+        print "You are approved!\n";
+        }elsif ($nota = 7){
+            print "You got average!\n";
+            }
